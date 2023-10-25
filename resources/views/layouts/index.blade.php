190f807049607eb8def546970c30e2d053c24de9
khoji-az1moff <128795575+khoji-az1moff@users.noreply.github.com>
2023-10-25 09:01:56 +0500

    stylelar boldi

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -49,7 +49,7 @@
 			</button>
 			<div class="collapse navbar-collapse" id="navbarSupportedContent">
 				<ul class="navbar-nav mx-auto mb-2 mb-lg-0">
-					<li class="nav-item"> <a class="nav-link" href="{{url('index')}}">Uy</a>
+					<li class="nav-item"> <a class="nav-link" href="">Uy</a>
 					</li>
 					<li class="nav-item "> <a class="nav-link" href="oqituvchilar.html">O'qituvchilar</a>
 					</li>
